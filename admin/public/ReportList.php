63c3095c107996517535762e540a7a826028dbb6
2201018 <148177579+2201018@users.noreply.github.com>
2024-05-13 10:55:30 +0900

    8

--- admin/public/ReportList.php
+++ admin/public/ReportList.php
@@ -23,10 +23,11 @@
         <?php
             // ユーザーIDが入力された場合の処理
             if(isset($_POST['search_id'])){
-                $sql=$conn->prepare('select * from Report where user_id like ?');
-                $sql->execute($_POST['search_id']);
+                $search_id = '%' . $_POST['search_id'] . '%'; // ワイルドカードを追加
+                $sql=$conn->prepare('SELECT * FROM Report WHERE user_id LIKE ?');
+                $sql->execute([$search_id]); // 配列に変更
              }else{ // 入力がない場合は全てのレポートを表示
-                $sql=$pdo->query('select * from Report');
+                $sql=$pdo->query('SELECT * FROM Report');
              }
 
             foreach($sql as $row) {
