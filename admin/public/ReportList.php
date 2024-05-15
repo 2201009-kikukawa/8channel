b925e17b100c3b363f35d5fb05148de2752f0020
2201018 <148177579+2201018@users.noreply.github.com>
2024-05-15 10:08:25 +0900

    8

--- admin/public/ReportList.php
+++ admin/public/ReportList.php
@@ -4,7 +4,7 @@
 ?>
 
 <body>
-    <form method="post" action="ReportList.php">
+    <form method="post">
         <input type="text" name="search_id" placeholder="IDを入力">
         <input type="submit" value="検索">
     </form>
