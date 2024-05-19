be6357ab4b29c44dfa7d626a11fb72a57257ba77
kikukawa <2201009@s.asojuku.ac.jp>
2024-05-20 01:38:13 +0900

    ホーム画面完成

---
+++ config/control/get_thread.php
@@ -0,0 +1,28 @@
+<?php 
+require '../../config/db-connect.php';
+
+function getThreads($pdo) {
+    $response = [];
+    try {
+        $stmt = $pdo->query("SELECT * FROM thread");
+        $threads = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($threads) {
+            foreach ($threads as $thread) {
+                $response[] = [
+                    'thread_id' => $thread['thread_id'],
+                    'thread_name' => $thread['thread_name'],
+                    'channel_id' => $thread['channel_id'],
+                    'views' => $thread['views'],
+                    'tag_id' => $thread['tag_id'],
+                    'date' => $thread['date'],
+                    'thread_txt' => $thread['thread_txt']
+                ];
+            }
+        }
+    } catch (PDOException $e) {
+        $response['error'] = $e->getMessage();
+    }
+
+    return json_encode($response);
+}
+?>
