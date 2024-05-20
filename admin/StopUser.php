b456366342ec9a533a171436e266528853dbdf61
2201018 <148177579+2201018@users.noreply.github.com>
2024-05-20 09:58:21 +0900

    7

---
+++ admin/StopUser.php
@@ -0,0 +1,51 @@
+<?php
+    require '../../config/db-connect.php';
+    require 'header.php';
+?>
+
+<body>
+    <form method="post">
+        <input type="text" name="search_id" placeholder="IDを入力">
+        <input type="submit" value="検索">
+    </form>
+        <input type=button value=タグ作成>
+        <br>
+        <a href="StopUser.php">停止アカウント一覧へ</a>
+        <a href="logout.php">ログアウト</a>
+    <table>
+        <tr>
+            <th>報告したユーザーID</th>
+            <th>報告されたユーザーID</th>
+            <th>報告内容</th>
+            <th>日付</th>
+        </tr>
+    
+        <?php
+        echo 1;
+        echo "<br>====<br>";
+        var_dump($_POST);
+        echo "<br>====<br>";
+            // ユーザーIDが入力された場合の処理
+            if(!empty($_POST['search_id'])){
+            echo 2;
+
+                $sql=$conn->prepare('select * from Report where report_user = ?');
+                $sql->execute([$_POST['search_id']]);
+                var_dump($sql);
+             }else{ // 入力がない場合は全てのレポートを表示
+            echo 3;
+                $sql=$conn->query('select * from Report');
+             }
+
+            foreach($sql as $row) {
+                echo '<tr>';
+                    echo '<td>', $row['report_id'], '</td>';
+                    echo '<td>', $row['report_user'], '</td>';
+                    echo '<td>', $row['user_id'], '</td>';
+                    echo '<td>', $row['data'], '</td>';
+                    echo '<td>', $row['report_reason'], '</td>';
+                echo '</tr>';
+            }
+        ?>
+    </table>
+</body>
