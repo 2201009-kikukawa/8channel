6b90e290cb0a4de9fc4357ca48b78c6108ab445e
masahiro <137256182+2201017@users.noreply.github.com>
2024-06-05 10:17:42 +0900

    変更

--- admin/login_input.php
+++ admin/login_input.php
@@ -1,16 +1,29 @@
+<?php
+require "header.php";
+?>
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <title>ログイン</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <h2>8ちゃんねる 管理者 ログイン</h2>
-    <form method="POST" action="login_output.php">
-        <label for="mail">mail:</label><br>
-        <input type="text" id="mail" name="mail" required><br>
-        <label for="password">パスワード:</label><br>
-        <input type="password" id="password" name="password" required><br>
-        <button type="submit" class="btn btn-primary">ログイン</button>
+    <h1 class="login-title">8ちゃんねる 管理者 ログイン</h1>
+    <div class="wave-container1">
+        <svg viewBox="0 0 1200 120" preserveAspectRatio="none" class="svg1">
+            <path d="M0,40 C500,100 700,0 1200,80" fill="none" stroke="lime" stroke-width="2"/>
+        </svg>
+    </div>
+    <form>
+        <label for="email">mail:</label>
+        <input type="email" id="email" name="email" value="aaa@bbb.com">
+        <br>
+        <label for="password">パスワード:</label>
+        <input type="password" id="password" name="password">
+        <br>
+        <button type="submit">ログイン</button>
     </form>
 </body>
 </html>
