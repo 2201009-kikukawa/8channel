a483a44d766dbde66fb9cd85813811a8150fe01d
2201018 <148177579+2201018@users.noreply.github.com>
2024-06-12 12:08:31 +0900

    1

--- admin/public/logout_output.php
+++ admin/public/logout_output.php
@@ -18,10 +18,10 @@
         echo "<p>ログアウトに失敗しました。</p>";
     }
     ?>
-    <p>ログイン画面に戻ります...</p>
+    <p>ユーザー画面に戻ります...</p>
     <script>
         setTimeout(function(){
-            window.location.href = './login_input.php'; // ログイン画面のURLにリダイレクト
+            window.location.href = '../../user/public/Top-index.php'; // ログイン画面のURLにリダイレクト
         }, 2000); // 2秒後にリダイレクト
     </script>
 </body>
