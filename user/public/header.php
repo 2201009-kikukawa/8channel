a1a1390e672a01c4bd6ea98636195b1c5d7e21c9
kikukawa <2201009@s.asojuku.ac.jp>
2024-06-18 11:09:38 +0900

    スタイル作成

--- user/public/header.php
+++ user/public/header.php
@@ -6,27 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>8channel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/header_style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <header class="bg-light p-3 border-bottom">
+    <header class="bg-black p-3 border-bottom">
         <div class="container d-flex justify-content-between align-items-center">
             <div>
                 <a href="Top-index.php"><img src="./image/rogo.jpg" alt="Site Logo" style="height: 50px;"></a>
             </div>
             
-            <div>
-                <h1 class="h4">8channel</h1>
-                <p class="mb-0 text-muted">Your gateway to unlimited channels</p>
+            <div class="header-text">
+                <h1 class="h1">8ちゃんねる</h1>
+                <p class="mb-0 text-muted" style="color: white !important;">ゲーム専門の掲示板やけん、ゲームに関する話をしろよぉ～</p>
             </div>
 
-            <div>
+            <div class="header-btn">
                 <?php if (isset($_SESSION['User']['id'])) :?>
-                    <a href="./logout.php" class="btn btn-outline-primary">Logout</a>
+                    <a href="./logout.php" class="btn btn-outline-primary hedbtn" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: 3.5rem; --bs-btn-font-size: .95rem;">Logout</a>
                 <?php else: ?>
-                    <a href="./login.php" class="btn btn-primary">Login</a>
+                    <a href="./login.php" class="btn btn-primary hedbtn" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: 3.5rem; --bs-btn-font-size: .95rem;">Login</a>
                 <?php endif; ?>
-                <a href="../../admin/public/login_input.php" class="btn btn-link">Admin Login</a>
+                <a href="../../admin/public/login_input.php" class="admin-link">Admin Login</a>
             </div>
         </div>
     </header>
