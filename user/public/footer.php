ecddd31c77c4e7c785505bb091332648ba80ea01
m1kqn <137014023+suzuki689@users.noreply.github.com>
2024-07-10 11:39:33 +0900

    フッターの修正２

--- user/public/footer.php
+++ user/public/footer.php
@@ -1,5 +1,5 @@
 <footer>
     <div>
-        <img class="Bottomline" src="./image/haikeiBottom.png">
+        <img class="Bottomline" src="./image/haikeiBottom.png" width="100%">
     </div>
 </footer>
